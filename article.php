d0f43d8fe6e2a21e36c3441dbb6d85d74d2d5e74
PaulLNG <paul.lange010@gmail.com>
2017-11-30 17:35:36 +0100

    Premier commit

---
+++ article.php
@@ -0,0 +1,4 @@
+<?php 
+
+
+echo ("Page ARTICLE BONJOUR !");
---
+++ index.php
@@ -0,0 +1,106 @@
+<!doctype html>
+<html lang="fr">
+    
+	<head>
+        <meta charset="utf-8">
+        <title> Portfolio | LANGE Paul </title>
+        <link rel="stylesheet" href="style2.css">
+        <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+        <script src="script.js"></script>
+    </head>
+    
+<body>
+	
+	
+	
+		<header> 
+		
+		<div class="bloc">
+			
+			
+			
+			
+			<div class="textHeader">
+				<div class="nameHeader"> LANGE Paul </div>
+				Développeur Web 
+				
+				<br><br>
+				
+				<br>
+				<ul>
+				  
+				  <li><a href="default.asp">Accueil</a></li>
+				  <li><a href="news.asp">Présentation</a></li>
+				  <li><a href="contact.asp">Curriculum Vitae</a></li>
+				  <li><a href="about.asp">Contact</a></li>
+				
+				</ul>
+				
+				
+			</div>
+			
+		
+			
+			
+		</header> 
+		
+		</div>
+		
+	
+	
+	<div class="bloc"> 
+	
+		
+	
+		<div class="presentation"> 
+		
+			<div class="bloc"> <h1> Présentation </h1> <br>
+			<hr color="#E33F0E" width="100px" size="6px"> 
+			<div class="blocImage">
+				<img src="sprite/paul.png" height="200"> 
+			</div> </div>
+			
+			<br> 
+			
+			Je m'appelle Paul LANGE , j'ai 20 ans , je suis actuellement étudiant en 3ème année à l'école IPSSI à 	Paris. <br>
+			L'école IPSSI est reconnu par l'état comme la meilleure école informatique de France. Un vote a été effectuer il y a de ça quelques jours et elle est arrivée en tête avec 3 votes de plus.
+		
+			<br><br><br>
+			
+			<div class="blocImage">
+			
+				<div class="bloc"> 
+			
+					
+				</div>
+			</div>
+			
+			
+			
+		</div>
+	
+	
+	
+	
+	
+	</div>	
+</body>
+		
+
+<footer>  
+	
+	<center>
+		
+		Site créé par LANGE Paul.
+			
+	</center>
+		
+
+</footer>
+
+
+</html>
+
+
+
+
