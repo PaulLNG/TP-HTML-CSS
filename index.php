3c3666292857abfcd3b5aaea2fe323bc654a29e2
PaulLNG <paul.lange010@gmail.com>
2017-12-01 12:43:02 +0100

    Commit

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 			
 			
 			<div class="textHeader">
-				<div class="nameHeader"> LANGE Paul </div>
+				<div class="nameHeader"> <b> LANGE Paul </b> </div>
 				Développeur Web 
 				
 				<br><br>
@@ -55,35 +55,90 @@
 		<div class="presentation"> 
 		
 			<div class="bloc"> <h1> Présentation </h1> <br>
-			<hr color="#E33F0E" width="100px" size="6px"> 
-			<div class="blocImage">
-				<img src="sprite/paul.png" height="200"> 
-			</div> </div>
-			
-			<br> 
-			
-			Je m'appelle Paul LANGE , j'ai 20 ans , je suis actuellement étudiant en 3ème année à l'école IPSSI à 	Paris. <br>
-			L'école IPSSI est reconnu par l'état comme la meilleure école informatique de France. Un vote a été effectuer il y a de ça quelques jours et elle est arrivée en tête avec 3 votes de plus.
-		
-			<br><br><br>
-			
-			<div class="blocImage">
-			
-				<div class="bloc"> 
-			
+				<div class="barreTitre">
+				
+						<hr color="#E33F0E" width="100px" size="4px"> 
+						</div>
+				<div class="flex">
+				<br><br>
+				
+				<div class="textPresentationSimple"> Je m'appelle Paul LANGE , j'ai 20 ans , je suis actuellement étudiant en 3ème année à l'école IPSSI à 	Paris. </div> <br> 
+				
+				<div class="blocImage">
+						<img src="sprite/paul.jpg" height="200"> 
+					</div> 
+					</div>
+			<div class="textPresentationDetail">L'informatique est un domaine qui me plait énormément , et ce , depuis mon plus jeune âge. Je veux faire de cette passion mon métier.
+​
+			Mon projet professionnel est de devenir chef de projet en informatique . Pour y arriver , j'aimerais faire une Licence MIAGE après mon BTS , en alternance ou en formation classique. Cette formation me permettrait d'acquérir une double compétence , en informatique et en gestion des entreprises.
+			Je ne compte pas m'arrêter après une licence MIAGE, j'aimerais préparer un Master 1 puis un Master 2 par la suite.
+			Ce cursus m'aidera à atteindre mon objectif : devenir Chef de Projet. </div>
 					
+		<br><br>
+		<center> <h2> Experience </h2> </center>
+				<div class="barreSousTitre">
+				<hr color="#E33F0E" width="30px" size="3px"> </div>
+			
+				<br>
+				<h3> 3 Janvier 2017 - 4 Février 2017 </h3> Stagiaire - Action Prévoyance  – Fontenay-sous-Bois (94120) <br> 
+				<em> Conception d’une solution logicielle(Tarificateur / Comparateur d’assurance) pour une agence de courtier (Php , Java , Javascript , Html et CSS utilisés) </em>
+				
+				<h3> Juin 2016 – Janvier 2017 </h3> Employé Polyvalent – McDonald’s – Chilly-Mazarin (91380)
+				<h3> 23 Mai 2016 – 1er Juillet 2016 </h3> Stagiaire – Stork Transport – Antony (92160) <br> 
+				<em> Réalisation d’un site Web pour une société de Transport (Php , Html et CSS utilisés) </em>
+				
+				
+				<br><br>
+				
+				<center> <h2> Formation</h2> </center>
+				<div class="barreSousTitre">
+				<hr color="#E33F0E" width="30px" size="3px"> </div> 
+				
+				<br>
+				<h3> 2015 </h3>Obtention du BAC PRO SEN(Systèmes Electroniques Numérique) avec Mention Bien – Lycée Edouard Branly 28100 DREUX
+				<h3> 2015 - 2016 </h3>Première année de BTS SIO(Services Informatiques aux Organisations) Option SLAM(Solutions Logicielles  & Applications Métiers) – Lycée Parc de Vilgénis 91300 MASSY
+				
+				
+				<h3> 2016 – 2017 </h3> Deuxième année de BTS SIO(Services Informatiques aux Organisations) Option SLAM(Solutions Logicielles  & Applications Métiers) – Lycée Parc de Vilgénis 91300 MASSY
+				
+				<h3> 2014 </h3> Obtention du BEP SEN(Systèmes Electroniques Numériques) avec 15,8/20 – Lycée Edouard Branly 28100 DREUX
+				
+				<br><br>
+				
+				<center> <h2> + D'infos </h2> </center>
+				<div class="barreSousTitre">
+				<hr color="#E33F0E" width="30px" size="3px"> 
 				</div>
+				
+				<br>
+				
+				<h3> Langages Informatique maîtrisés </h3> Php , C# , C , Java , Html , SQL , CSS...
+				<h3>Gestion de base de données </h3> MySql 
+				<h3> Logiciels maîtrisés </h3> Microsoft Visual Studio , NetBeans , NotePad ++ , EasyPHP , Wamp , Atom , Pack Office…
+				<h3> Langues </h3> Anglais (Niveau Intermédiaire)
+				<h3> Centres d'Intérêt </h3> Informatique , Sport , Musculation, Cinéma.
+				
+				<br><br>
+					
+					<div class="button">
+						<a href="CV.pdf" target="_blank">
+							<input type="button" value="Télécharger en format PDF">
+						</a>
+					</div>
+					
+				
+			
+			
+			
+			
 			</div>
-			
-			
-			
 		</div>
 	
+	</div>
 	
 	
 	
-	
-	</div>	
+		
 </body>
 		
 
@@ -91,7 +146,7 @@
 	
 	<center>
 		
-		Site créé par LANGE Paul.
+		© 2017 by Paul LANGE.
 			
 	</center>
 		
