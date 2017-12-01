690082adeddc97475e46d6b2cdc2ada982e18325
PaulLNG <paul.lange010@gmail.com>
2017-12-01 16:57:18 +0100

    Last Commit 16H57

--- index.php
+++ index.php
@@ -5,29 +5,23 @@
         <meta charset="utf-8">
         <title> Portfolio | LANGE Paul </title>
         <link rel="stylesheet" href="style2.css">
-        <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
-        <script src="script.js"></script>
+        <!-- <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css"> -->
     </head>
     
 <body>
 	
 	
 	
-		<header> 
-		
+	<header> 
+	
 		<div class="bloc">
-			
-			
-			
-			
 			<div class="textHeader">
 				<div class="nameHeader"> <b> LANGE Paul </b> </div>
 				Développeur Web 
 				
-				<br><br>
+				<br><br><br>
 				
-				<br>
-				<ul>
+				<ul id="navbar">
 				  
 				  <li><a href="#experience">Expériences</a></li>
 				  <li><a href="#formation">Formations</a></li>
@@ -35,50 +29,52 @@
 				  <li><a href="#contact">Contact</a></li>
 				
 				</ul>
-				
-				
 			</div>
-			
+		</div>	
+	</header> 
 		
-			
-			
-		</header> 
+	
+<div class="bloc"> 
+	
 		
+	
+	<div class="presentation"> 
+		
+		<h1> Présentation </h1> <br>
+		<div class="barreTitre">
+				
+			<hr color="#E33F0E" width="100px" size="4px"> 
+			
 		</div>
 		
-	
-
-	<div class="bloc"> 
-	
-		
-	
-		<div class="presentation"> 
-		
-			<h1> Présentation </h1> <br>
-				<div class="barreTitre">
+		<div class="flex">
+			<br><br>
 				
-						<hr color="#E33F0E" width="100px" size="4px"> 
-						</div>
-				<div class="flex">
-				<br><br>
-				
-				<div class="textPresentationSimple"> Je m'appelle Paul LANGE , j'ai 20 ans , je suis actuellement étudiant en 3ème année à l'école IPSSI à 	Paris. </div> <br> 
+			<div class="textPresentationSimple"> Je m'appelle Paul LANGE , j'ai 20 ans , je suis actuellement étudiant en 3ème année à l'école IPSSI à 	Paris. </div> <br> 
 				
 				<div class="blocImage">
-						<img src="sprite/paul.jpg" height="200"> 
-					</div> 
-					</div>
-			<div class="textPresentationDetail">L'informatique est un domaine qui me plait énormément , et ce , depuis mon plus jeune âge. Je veux faire de cette passion mon métier. <br> <br>
-​
-			Mon projet professionnel est de devenir chef de projet en informatique . Pour y arriver , j'aimerais faire une Licence MIAGE après mon BTS , en alternance ou en formation classique. <br> Cette formation me permettrait d'acquérir une double compétence , en informatique et en gestion des entreprises.
+					<img src="sprite/paul.jpg" height="200"> 
+				</div> 
+		</div>
+		
+		<div class="textPresentationDetail">
+			L'informatique est un domaine qui me plait énormément , et ce , depuis mon plus jeune âge. Je veux faire de cette passion mon métier. <br> <br>
+​			Mon projet professionnel est de devenir chef de projet en informatique . Pour y arriver , j'aimerais faire une Licence MIAGE après mon BTS , 
+			en alternance ou en formation classique. <br> Cette formation me permettrait d'acquérir une double compétence , en informatique et en gestion des entreprises.
 			Je ne compte pas m'arrêter après une licence MIAGE, j'aimerais préparer un Master 1 puis un Master 2 par la suite. <br> <br>
 			Ce cursus m'aidera à atteindre mon objectif : devenir Chef de Projet. </div>
 					
 		<br><br>
-		<center> <h2 id="experience"> Experiences </h2> 
-				<div class="barreSousTitre">
-				<hr color="#E33F0E" width="30px" size="3px"> </div>
-				<div class="blocExperience">
+		<center> 
+			
+			<h2 id="experience"> Experiences </h2> 
+			<div class="barreSousTitre">
+				
+				<hr color="#E33F0E" width="30px" size="3px"> 
+			</div>
+				
+			<div class="blocExperience">
+				
 				<br>
 				<h3> 3 Janvier 2017 - 4 Février 2017 </h3> Stagiaire - Action Prévoyance  – Fontenay-sous-Bois (94120) <br> 
 				<em> Conception d’une solution logicielle(Tarificateur / Comparateur d’assurance) pour une agence de courtier (Php , Java , Javascript , Html et CSS utilisés) </em>
@@ -86,14 +82,19 @@
 				<h3> Juin 2016 – Janvier 2017 </h3> Employé Polyvalent – McDonald’s – Chilly-Mazarin (91380)
 				<h3> 23 Mai 2016 – 1er Juillet 2016 </h3> Stagiaire – Stork Transport – Antony (92160) <br> 
 				<em> Réalisation d’un site Web pour une société de Transport (Php , Html et CSS utilisés) </em>
-				</div>
+				
+			</div>
 				
 				<br><br>
 				
-				<h2 id="formation"> Formations </h2>
-				<div class="barreSousTitre">
-				<hr color="#E33F0E" width="30px" size="3px"> </div> 
-				<div class="blocFormations">
+			<h2 id="formation"> Formations </h2>
+			<div class="barreSousTitre">
+				
+				<hr color="#E33F0E" width="30px" size="3px"> 
+				
+			</div> 
+			
+			<div class="blocFormations">
 				<br>
 				
 				<h3> 2017 - 2018 </h3> Première année BAC +3 Info <br> Ecole IPSSI 75000 Paris
@@ -101,16 +102,20 @@
 				<h3> 2015 </h3>Obtention du BAC PRO SEN(Systèmes Electroniques Numérique) avec Mention Bien <br> Lycée Edouard Branly 28100 DREUX
 				<h3> 2014 </h3> Obtention du BEP SEN(Systèmes Electroniques Numériques) avec 15,8/20 <br> Lycée Edouard Branly 28100 DREUX
 				
-				</div>
+			</div>
+				
 				<br><br>
 				
-				<h2 id="infosupp"> + D'infos </h2> 
-				<div class="barreSousTitre">
+			<h2 id="infosupp"> + D'infos </h2> 
+			<div class="barreSousTitre">
+				
 				<hr color="#E33F0E" width="30px" size="3px"> 
-				</div>
+				
+			</div>
 				
 				<br>
-				<div class="blocInfos">
+			
+			<div class="blocInfos">
 				
 				
 					
@@ -119,16 +124,20 @@
 					<h3> Logiciels maîtrisés </h3> Microsoft Visual Studio , NetBeans , NotePad ++ , EasyPHP , Wamp , Atom , Pack Office…
 					<h3> Langues </h3> Anglais (Niveau Intermédiaire)
 					<h3> Centres d'Intérêt </h3> Informatique , Sport , Musculation, Cinéma.
-				</div>	
+			</div>	
+				
 				<br><br>
 				
-					<div class="button">
-						<a href="CV.pdf" target="_blank">
-							<input type="button" value="Télécharger en format PDF">
-						</a>
-					</div>
+			<div class="button">
+				
+				<a href="CV.pdf" target="_blank">
 					
-				</center>
+					<input type="button" value="Télécharger en format PDF">
+					
+				</a>
+			</div>
+					
+		</center>
 			
 			
 			
@@ -148,9 +157,26 @@
 		<div id="contact" class="blocContact"> <h2> Contact </h2>
 		
 			<div class="barreSousTitre">
+				
 				<hr color="#E33F0E" width="30px" size="3px"> 
 			
 			</div>
+			
+			<br>
+			<h3> Adresse : 176 boulevard des Aviateurs Alliés <br> 95610 Eragny sur Oise </h3> 
+			<h3> Numéro de téléphone : 06 37 29 49 18 </h3>
+			<h3> Email : paul.lange@10gmail.com </h3> <br>
+			<h3 id="social-network"> - Réseaux sociaux - </h3> <br> <br>
+			<div class="icone"> 
+			
+				<a href="https://www.facebook.com/"> <div class="fb"></div> </a>
+				<a href="https://www.linkedin.com/"> <div class="linkedin"></div> </a>
+				<a href="https://www.twitter.com/"> <div class="twitter"></div> </a>
+				<a href="https://www.youtube.com/"> <div class="youtube"></div> </a>
+				
+			</div>
+			
+		
 		</div>
 	
 	</div>
@@ -160,9 +186,6 @@
 </center>
 	
 <footer>  
-	
-	
-	
 	
 	<center>
 		
